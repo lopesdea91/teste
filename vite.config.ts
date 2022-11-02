61b6b766e33d374667803b06ad7a947e3210c046
lopesdea91 <lopesdea91@gmail.com>
2022-11-02 15:00:05 -0300

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -9,19 +9,42 @@ export default defineConfig({
     vue(),
     VitePWA({
       injectRegister: 'auto',
-      registerType: 'autoUpdate', 
-      // devOptions: {
-      //   enabled: true
-      // }
+      registerType: 'autoUpdate',
+      manifest: {
+        name: "App for tests",
+        short_name: "AppTest",
+        description: "App for tests",
+        theme_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ],
+        background_color: "#ffffff",
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        sourcemap: true
+      },
     })
   ],
   resolve: {
     alias: {
+      // "@": path.resolve("./src"),
       '@': path.resolve(__dirname, './src'),
     },
   },
   server: {
     port: 3000,
+    host: true,
   },
   build: {
     outDir: 'docs'
